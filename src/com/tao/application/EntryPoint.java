b6704d71e0561c97b5ae6248b0dd7add650e268d
Tao Zhang <tzhang@gracenote.com>
2016-02-11 13:37:21 -0600

    mongo tutorial

--- src/com/tao/application/EntryPoint.java
+++ src/com/tao/application/EntryPoint.java
@@ -3,7 +3,7 @@ package com.tao.application;
 public class EntryPoint {
 
 	public static void main(String[] args) {
-		System.out.println("Start Application...");
+
 	}
 
 }
---
+++ src/com/tao/application/KafkaTutorial.java
@@ -0,0 +1,5 @@
+package com.tao.application;
+
+public class KafkaTutorial {
+
+}

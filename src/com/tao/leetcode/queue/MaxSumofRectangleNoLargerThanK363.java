457162bdee5b55c8d1da559d45d555fe5e058324
Tao Zhang <tzhang@gracenote.com>
2016-09-15 16:17:15 -0500

    max sub array less than k

---
+++ src/com/tao/leetcode/queue/MaxSumofRectangleNoLargerThanK363.java
@@ -0,0 +1,15 @@
+package com.tao.leetcode.queue;
+
+public class MaxSumofRectangleNoLargerThanK363 {
+
+	public static void main(String[] args) {
+		int[] array={31,-41,59,26,-53,58,97,-93,-23,84};
+		int k=187;
+		int result=maxSubArray(array, k);
+		System.out.println(result);
+	}
+	
+	public static int maxSubArray(int[] array,int k){
+		return 0;
+	}
+}

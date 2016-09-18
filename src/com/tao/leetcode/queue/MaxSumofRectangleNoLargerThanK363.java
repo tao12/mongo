1f90d5f65fadccf5aeb80d0ad4f9a1272b8fc606
Tao Zhang <tzhang@gracenote.com>
2016-09-18 13:39:33 -0500

    leetcode

--- src/com/tao/leetcode/queue/MaxSumofRectangleNoLargerThanK363.java
+++ src/com/tao/leetcode/queue/MaxSumofRectangleNoLargerThanK363.java
@@ -5,35 +5,67 @@ import java.util.TreeSet;
 public class MaxSumofRectangleNoLargerThanK363 {
 
 	public static void main(String[] args) {
-		int[] array = { 31, -41, 59, 26, -53, 58, 97, -93, -23, 84 };
-		int k = 188;
-		int result = maxSubArray(array, k);
+		int[][] matrix = { { 1, 0, -1 }, { 0, -2, 3 } };
+		int k = 2;
+		int result = maxSumSubmatrix(matrix, k);
 		System.out.println(result);
-
-		// TreeSet<Integer> set=new TreeSet<>();
-		// for(Integer i:array){
-		// set.add(i);
-		// }
-		//
-		// System.out.println(set.ceiling(32));
 	}
 
-	public static int maxSubArray(int[] array, int k) {
-		if (array.length == 0)
+	public static int maxSumSubmatrix(int[][] matrix, int k) {
+		// 2D Kadane's algorithm + 1D maxSum problem with sum limit k
+		// 2D subarray sum solution
+
+		// boundary check
+		if (matrix.length == 0)
 			return 0;
 
-		TreeSet<Integer> set = new TreeSet<>();
-		int currSum = 0;
+		int m = matrix.length, n = matrix[0].length;
 		int result = Integer.MIN_VALUE;
-		for (int i = 0; i < array.length; i++) {
-			currSum += array[i];
-			Integer num = set.ceiling(currSum - k);
-			System.out.println("num:"+num);
-			if (num != null) {
-				result = Math.max(result, currSum - num);
+
+		// outer loop should use smaller axis
+		// now we assume we have more rows than cols, therefore outer loop will
+		// be based on cols
+		for (int left = 0; left < n; left++) {
+			// array that accumulate sums for each row from left to right
+			int[] sums = new int[m];
+			for (int right = left; right < n; right++) {
+				// update sums[] to include values in curr right col
+				for (int i = 0; i < m; i++) {
+					sums[i] += matrix[i][right];
+				}
+
+				System.out.print("sums:");
+				for (int i = 0; i < sums.length; i++) {
+					System.out.print(sums[i] + ",");
+				}
+				System.out.println();
+
+				// we use TreeSet to help us find the rectangle with maxSum <= k
+				// with O(logN) time
+				TreeSet<Integer> set = new TreeSet<Integer>();
+				// add 0 to cover the single row case
+				set.add(0);
+				int currSum = 0;
+
+				for (int sum : sums) {
+					currSum += sum;
+					// we use sum subtraction (curSum - sum) to get the subarray
+					// with sum <= k
+					// therefore we need to look for the smallest sum >= currSum
+					// - k
+					System.out.println("current sum:" + currSum);
+					System.out.println("set:" + set.toString());
+
+					Integer num = set.ceiling(currSum - k);
+					System.out.println("ceiling num:" + num);
+					if (num != null)
+						result = Math.max(result, currSum - num);
+					System.out.println("result:" + result);
+					set.add(currSum);
+				}
 			}
-			set.add(currSum);
 		}
+
 		return result;
 	}
 }

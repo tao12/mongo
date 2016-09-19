11dd6760ef15743678e090c7b46af5488330a1e7
Tao Zhang <tzhang@gracenote.com>
2016-09-19 09:57:44 -0500

    HashMap

--- src/com/tao/algorithm/HashMap.java
+++ src/com/tao/algorithm/HashMap.java
@@ -1,5 +1,12 @@
 package com.tao.algorithm;
 
 public class HashMap {
-
+	public interface Map<K,V>{
+		int size();
+		public V get(K key);
+		public void put(K key, V value);
+		public boolean contains(K key);
+		public void remove(K key);
+		Iterable<K> keySet();
+	}
 }

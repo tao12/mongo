14d383ab5c52e0d6e4bde1f8ab3956331761da2a
Tao Zhang <tzhang@gracenote.com>
2016-09-20 16:36:47 -0500

    queue

---
+++ src/com/tao/leetcode/stack/ImplementQueueusingStacks232.java
@@ -0,0 +1,26 @@
+package com.tao.leetcode.stack;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class ImplementQueueusingStacks232 {
+	static Stack<Integer> in=new Stack<>();
+	Stack<Integer> out=new Stack<>();
+	static Queue<Integer> q=new LinkedList<>();
+	
+	public static void main(String[] args) {
+		q.size();
+		q.poll();
+		q.add(1);
+		q.peek();
+		q.contains(1);
+		q.size();
+		
+		in.push(1);
+		in.pop();
+		in.peek();
+		in.contains(1);
+		
+	}
+}
